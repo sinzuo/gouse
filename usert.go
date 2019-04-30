6d9b5146072423e94b1bf06b9235d269e9b005c6
root <1586706912@qq.com>
2019-04-30 14:05:57 +0800

    1.可以读写文件 和 log 操作

--- usert.go
+++ usert.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 )
 
+var logok *log.Logger
+
+func init() {
+	logok = log.New(os.Stdout, "", log.LstdFlags)
+}
+
 func main() {
+
 	jiang := strings.Fields("jiang yi bo ")
-	fmt.Println("jiangyibo" + jiang[2])
+	ss, err := ioutil.ReadFile("/work/gouse/jiang.txt")
+	if err != nil {
+		fmt.Println("oo")
+	}
+	logok.Println("mmm")
+	fmt.Println("jiangyibo" + jiang[2] + string(ss))
 }
