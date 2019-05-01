b2ac9abee2a7e990e063d105c3981347552afb97
root <1586706912@qq.com>
2019-05-01 11:32:42 +0800

    1.xiugai

--- usert.go
+++ usert.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+
 	"os"
+	"reflect"
 	"strings"
+	"time"
+
+	"github.com/bitly/go-simplejson"
 )
 
 var logok *log.Logger
@@ -14,13 +19,42 @@ func init() {
 	logok = log.New(os.Stdout, "", log.LstdFlags)
 }
 
+type A struct {
+	Bt int
+}
+
+func (a *A) Str() {
+	fmt.Println("ok")
+}
+
+const (
+	ss int = iota
+	mm
+)
+
+type D interface {
+	Open()
+}
+
 func main() {
 
 	jiang := strings.Fields("jiang yi bo ")
+	ccd := reflect.TypeOf(ss)
+	reflect.ValueOf(1)
+	fmt.Println(ccd.Name() + os.Args[0] + time.Now().Format("2017:12:1"))
+	sm, _ := simplejson.NewJson([]byte("{\"jinag\":\"bo\"}"))
+
 	ss, err := ioutil.ReadFile("/work/gouse/jiang.txt")
 	if err != nil {
+
 		fmt.Println("oo")
+		defer fmt.Println("rrr")
+		return
+
 	}
-	logok.Println("mmm")
+     ii chan
+	oooo, _ := sm.String()
+	logok.Println("mmm" + oooo)
+
 	fmt.Println("jiangyibo" + jiang[2] + string(ss))
 }
